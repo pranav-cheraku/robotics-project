ebc8c580a324c1810120657367789eaa206879cb
Jeffrey-F-Guo <jeffrey.guo00703@gmail.com>
2026-02-26 16:24:22 -0800

    Adding vision code

---
+++ train_yolo.py
@@ -0,0 +1,21 @@
+from ultralytics import YOLO
+
+# Load a COCO-pretrained YOLOv8n model
+model = YOLO("yolov8n.pt")
+
+# Display model information (optional)
+model.info()
+
+# Load data
+
+
+# Train the model on the COCO8 example dataset for 100 epochs
+results = model.train(data="dataset/train", epochs=100, imgsz=640)
+
+
+# Test loop
+# Run inference with the YOLOv8n model on the 'bus.jpg' image
+results = model("path/to/bus.jpg")
+
+
+# save model
